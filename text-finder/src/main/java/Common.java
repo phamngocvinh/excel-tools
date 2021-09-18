8c4f42de4dbf1040d72b890f92107afdd2fcbb89
Pham Ngoc Vinh <vinhgalaxys2@gmail.com>
2021-09-18 17:12:27 +0700

    add properties config

---
+++ text-finder/src/main/java/Common.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+public class Common {
+	/**
+	 * Check if String is null or blank empty
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static boolean isNullOrEmpty(String str) {
+
+		if (str == null || str.length() == 0) {
+			return true;
+		}
+
+		return false;
+	}
+}
