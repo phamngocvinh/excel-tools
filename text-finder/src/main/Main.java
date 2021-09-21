1165fce51a843bb3bdd0a91fcf667102278ba12c
Pham Ngoc Vinh <vinhgalaxys2@gmail.com>
2021-09-21 11:40:55 +0700

    Update Main.java

--- text-finder/src/main/Main.java
+++ text-finder/src/main/Main.java
@@ -3,17 +3,26 @@ package main;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -57,7 +66,22 @@ public class Main {
 	/**
 	 * Config: Search Conditions
 	 */
-	private static List<String> config_SrchCond = new ArrayList<>();
+	private static List<String> config_SrchCond = new LinkedList<>();
+
+	/**
+	 * Result workbook
+	 */
+	private static XSSFWorkbook wb_Result;
+
+	/**
+	 * DataFormatter
+	 */
+	private static DataFormatter formatter = new DataFormatter();
+
+	/**
+	 * List Result
+	 */
+	private static List<String> listResult = new LinkedList<>();
 
 	/**
 	 * Main class
@@ -85,21 +109,113 @@ public class Main {
 
 		// Read config file
 		try {
-			readConfig();
-		} catch (Exception e) {
-			logger.error(e.getMessage());
+			if (!readConfig()) {
+				return;
+			}
+		} catch (InvalidFormatException e1) {
+			logger.error("Config file InvalidFormatException");
 			return;
+		} catch (IOException e1) {
+			logger.error("Config file IOException");
+			return;
+		}
+
+		// Initialize Result workbook
+		wb_Result = new XSSFWorkbook();
+		wb_Result.createSheet("Result");
+
+		// If search path is folder
+		if (config_IsFolder) {
+			Collection<File> fileList = FileUtils.listFiles(new File(config_search_path), null, false);
+			for (File file : fileList) {
+				doSearch(file);
+			}
+		} else {
+			// If search path is file
+			doSearch(new File(config_search_path));
+		}
+		
+		writeResult();
+
+		FileOutputStream outputStream;
+		try {
+			outputStream = new FileOutputStream("Result.xlsx");
+			wb_Result.write(outputStream);
+		} catch (FileNotFoundException e) {
+			logger.error("Write Result FileNotFoundException");
+		} catch (IOException e) {
+			logger.error("Write Result IOException");
+		}
+	}
+	
+	/**
+	 * Write Result to Workbook
+	 */
+	private static void writeResult() {
+		
+		XSSFSheet sheet = wb_Result.getSheet("Result");
+		
+		for (int idx = 0; idx < listResult.size(); idx++) {
+			String[] arr = listResult.get(idx).split("&%&");
+			
+			sheet.createRow(idx);
+			sheet.getRow(idx).createCell(0).setCellValue(arr[0]);
+			sheet.getRow(idx).createCell(1).setCellValue(arr[1]);
+			sheet.getRow(idx).createCell(2).setCellValue(arr[2]);
+			sheet.getRow(idx).createCell(3).setCellValue(arr[3]);
+		}
+	}
+
+	/**
+	 * Search Process
+	 * 
+	 * @param file File
+	 */
+	private static void doSearch(File file) {
+		try {
+			String ext = FilenameUtils.getExtension(file.getName());
+			Workbook workbook;
+			if (ext.equals("xlsx")) {
+				workbook = new XSSFWorkbook(new FileInputStream(file));
+			} else if (ext.equals("xls")) {
+				workbook = new HSSFWorkbook(new FileInputStream(file));
+			} else {
+				logger.error("File not Supported: " + file.getName());
+				return;
+			}
+
+			for (int idx = 0; idx < workbook.getNumberOfSheets(); idx++) {
+				Sheet sheet = workbook.getSheetAt(idx);
+				for (int rIdx = 0; rIdx < sheet.getLastRowNum(); rIdx++) {
+					if (sheet.getRow(rIdx + 1) != null) {
+						for (int cIdx = 0; cIdx < sheet.getRow(rIdx + 1).getLastCellNum(); cIdx++) {
+							String cellVal = formatter.formatCellValue(sheet.getRow(rIdx + 1).getCell(cIdx));
+							if (!StringUtils.isBlank(cellVal)) {
+								for (String srchCond : config_SrchCond) {
+									if (srchCond.equals(cellVal)) {
+										listResult.add(srchCond + "&%&" + (cIdx + 1) + "&%&" + (rIdx + 2) + "&%&" + file.getName());
+										logger.info("Found: " + srchCond + " Location: " + (cIdx + 1) + " " + (rIdx + 2));
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		} catch (IOException e) {
+			logger.error("IOException: " + file.getName());
 		}
 	}
 
 	/**
 	 * Read Config File to get settings
 	 * 
+	 * @throws IOException
+	 * @throws InvalidFormatException
+	 * 
 	 * @throws Exception
 	 */
-	private static void readConfig() throws Exception {
-
-		DataFormatter formatter = new DataFormatter();
+	private static boolean readConfig() throws InvalidFormatException, IOException {
 
 		// Check if config excel exists
 		File fileWorkBook = new File(dir + prop.getProperty("config.file"));
@@ -107,7 +223,7 @@ public class Main {
 			logger.info("Read config.xlsx");
 		} else {
 			logger.error("Config file not exists");
-			return;
+			return false;
 		}
 
 		// Open config file
@@ -118,8 +234,9 @@ public class Main {
 
 		// Check if config sheet exists
 		if (sheet == null) {
+			logger.error("Config sheet not found");
 			workbook.close();
-			throw new Exception("Config sheet not found");
+			return false;
 		}
 
 		// Get search path
@@ -130,6 +247,11 @@ public class Main {
 		// Check if search path is file or folder
 		if (Files.isDirectory(Path.of(config_search_path))) {
 			config_IsFolder = true;
+			if (Path.of(config_search_path).toFile().listFiles().length == 0) {
+				logger.info("Search path is empty");
+				workbook.close();
+				return false;
+			}
 			logger.info("Search in " + config_search_path);
 		} else if (Files.isRegularFile(Path.of(config_search_path))) {
 			config_IsFolder = false;
@@ -137,7 +259,7 @@ public class Main {
 		} else {
 			logger.error("Search path not exist. Check config.xlsx");
 			workbook.close();
-			return;
+			return false;
 		}
 
 		// Get Search Condition
@@ -151,14 +273,17 @@ public class Main {
 		logger.info("Search condition: " + config_SrchCond);
 
 		workbook.close();
+
+		return true;
 	}
 
 	/**
 	 * Check if Config file is valid
+	 * 
 	 * @return
 	 */
 	private static boolean isValidConfig() {
-		
+
 		// Check if correctly config
 		if (StringUtils.isBlank(getProp("app.version"))) {
 			logger.error("Config: Missing app.version property");
