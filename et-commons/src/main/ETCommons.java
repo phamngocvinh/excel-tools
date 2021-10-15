e670d8aff07e3e3cd11b08f2d95c7a0f4084db63
Pham Ngoc Vinh <vinhgalaxys2@gmail.com>
2021-10-15 10:00:46 +0700

    Add common project

---
+++ et-commons/src/main/ETCommons.java
@@ -0,0 +1,55 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ETCommons {
+
+	public static enum Project {
+		TEXT_FINDER("text-finder"), DIFF_FINDER("diff-finder");
+
+		private String name;
+
+		private Project(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+	}
+
+	/**
+	 * Get latest version
+	 * 
+	 * @param projectName
+	 * @return
+	 */
+	public static String getLatestVersion(Project project) {
+		try {
+			// Get latest version
+			URL url = new URL("https://api.github.com/repos/phamngocvinh/excel-tools/releases");
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+			String line = in.readLine();
+			in.close();
+
+			// Get release version
+			Pattern p = Pattern.compile(".+?name.+?" + project.getName() + "-v(.+).zip.+");
+			Matcher m = p.matcher(line);
+			boolean isMatch = m.matches();
+
+			if (isMatch) {
+				return m.group(1).substring(0, m.group(1).indexOf(".zip"));
+			}
+
+			return null;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
--- text-finder/src/main/Main.java
+++ text-finder/src/main/Main.java
@@ -1,13 +1,10 @@
 package main;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
@@ -16,8 +13,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
@@ -212,12 +207,10 @@ public class Main {
 			logger.info(StringUtils.rightPad("=== END ", LOG_NUM, "="));
 			if (isNewVersionExists) {
 				logger.info(StringUtils.rightPad("=== New Version Availiable ", LOG_NUM, "="));
-				logger.warn(
-						"You're using older version. Please update to the latest version in the link below");
+				logger.warn("You're using older version. Please update to the latest version in the link below");
 				logger.info("Current: v" + appVersion);
 				logger.info("Latest: v" + netVersion);
-				logger.info("Official Link: https://github.com/phamngocvinh/excel-tools/releases/tag/v"
-						+ netVersion);
+				logger.info("Official Link: https://github.com/phamngocvinh/excel-tools/releases/tag/v" + netVersion);
 			}
 		}
 	}
@@ -227,30 +220,13 @@ public class Main {
 
 		Thread thread = new Thread() {
 			public void run() {
-				try {
-					// Get latest version
-					URL url = new URL("https://api.github.com/repos/phamngocvinh/excel-tools/releases");
 
-					BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+				// Get latest version
+				netVersion = ETCommons.getLatestVersion(ETCommons.Project.TEXT_FINDER);
 
-					String line = in.readLine();
-					in.close();
-
-					// Get release version
-					Pattern p = Pattern.compile(".+?name.+?text-finder-v(.+).zip.+");
-					Matcher m = p.matcher(line);
-					boolean isMatch = m.matches();
-
-					if (isMatch) {
-						netVersion = m.group(1).substring(0, m.group(1).indexOf(".zip"));
-
-						// If local version is older than newest version
-						if (netVersion.compareTo(appVersion) > 0) {
-							isNewVersionExists = true;
-						}
-					}
-				} catch (Exception e) {
-					logger.error("Internal Exception: " + e.getLocalizedMessage());
+				// If local version is older than newest version
+				if (netVersion.compareTo(appVersion) > 0) {
+					isNewVersionExists = true;
 				}
 			}
 		};
